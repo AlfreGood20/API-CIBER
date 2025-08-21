ad7f74614a52b924cc56ba4d53db9f6215fd37eb
José Alfredo López De La Cruz <josealfredolopezdelacruz2@gmail.com>
2025-08-21 00:36:18 +0000

    Exeption de recursos no disponibles

---
+++ src/main/java/io/github/alfredgood/api_ciber/excepciones/RecursoNoDisponible.java
@@ -0,0 +1,9 @@
+package io.github.alfredgood.api_ciber.excepciones;
+
+public class RecursoNoDisponible extends RuntimeException{
+
+    public RecursoNoDisponible(String mensaje){
+        super(mensaje);
+    }
+
+}
