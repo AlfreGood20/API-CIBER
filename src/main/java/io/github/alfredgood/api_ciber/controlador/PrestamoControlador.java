56b34d37364a1e7002046d555e6b20b9a2ab6ff1
José Alfredo López De La Cruz <josealfredolopezdelacruz2@gmail.com>
2025-08-21 00:34:28 +0000

    Controlador de prestamos

---
+++ src/main/java/io/github/alfredgood/api_ciber/controlador/PrestamoControlador.java
@@ -0,0 +1,48 @@
+package io.github.alfredgood.api_ciber.controlador;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.github.alfredgood.api_ciber.modelo.dto.create.PrestamoCreateDTO;
+import io.github.alfredgood.api_ciber.modelo.dto.response.PrestamoDTO;
+import io.github.alfredgood.api_ciber.servicio.PrestamoServ;
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class PrestamoControlador {
+
+    private final PrestamoServ servicio;
+
+    public PrestamoControlador(PrestamoServ servicio) {
+        this.servicio = servicio;
+    }
+    
+
+    @PostMapping("/prestamo")
+    public ResponseEntity<PrestamoDTO> nuevoPrestamo(@Valid @RequestBody PrestamoCreateDTO create) {
+        return new ResponseEntity<PrestamoDTO>(servicio.nuevo(create), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/prestamos")
+    public ResponseEntity<List<PrestamoDTO>> listaDePrestamos() {
+        return ResponseEntity.ok().body(servicio.listaCompleta());
+    }
+
+    @PatchMapping("/prestamo/{id}")
+    public ResponseEntity<Void> devolverPrestamo (@PathVariable long id){
+        servicio.devolver(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    
+
+}
