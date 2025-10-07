0614f38628f30a77d7881e1ee89eb5b2c06b29c7
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-10-06 21:18:03 -0600

    Primeras configuraciones de la documentacion

--- src/main/java/io/github/alfredgood/api_ciber/ApiCiberApplication.java
+++ src/main/java/io/github/alfredgood/api_ciber/ApiCiberApplication.java
@@ -3,6 +3,29 @@ package io.github.alfredgood.api_ciber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API CIBER",
+		description = "Bienvenido a mi api ciber, hecha con spring boot 3.5.4",
+		termsOfService = "",
+		version = "1.0.1",
+		contact = @Contact(
+			name = "José Alfredo López De La Cruz",
+			url = "https://github.com/AlfreGood20",
+			email = "josealfredolopezdelacruz2@gmail.com"
+		)
+	),
+
+	servers = {
+		@Server (url = "http://localhost:8080/", description = "Aqui esta todos los enpoint de la api, UNICO server")
+	}
+)
+
 @SpringBootApplication
 public class ApiCiberApplication {
 
